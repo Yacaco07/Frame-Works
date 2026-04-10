7d8473ab9905ec55862be2351816b19837b982db
VitorSLima <dev.vitorlima@gmail.com>
2026-04-10 12:10:52 -0400

    Refactor Todos module and controller; update Prisma client usage and add repository pattern for better structure

--- backend/src/todos/todos.module.ts
+++ backend/src/todos/todos.module.ts
@@ -1,9 +1,36 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { TodosService } from './todos.service';
 import { TodosController } from './todos.controller';
+import { PrismaService } from 'src/shared/databases/prisma.database';
+import {
+  CreateTodoRepository,
+  DeleteTodoRepository,
+  FindAlltodosRepository,
+  FindTodoByIdRepository,
+  UpdateTodoRepository,
+} from './repository';
+import { CreateTodoUseCase } from './use-cases/create-todo.use-case';
+import { FindAllTodosUseCase } from './use-cases/find-all-todos.use-case';
+import { FindTodoByIdUseCase } from './use-cases/find-todo-by-id.use-case';
+import { UpdateTodoUseCase } from './use-cases/update-todo.use-case';
+import { DeleteTodoUseCase } from './use-cases/delete-todo.use-case';
 
 @Module({
   controllers: [TodosController],
-  providers: [TodosService],
+  providers: [
+    Logger,
+    PrismaService,
+    TodosService,
+    CreateTodoRepository,
+    DeleteTodoRepository,
+    FindAlltodosRepository,
+    FindTodoByIdRepository,
+    UpdateTodoRepository,
+    CreateTodoUseCase,
+    FindAllTodosUseCase,
+    FindTodoByIdUseCase,
+    UpdateTodoUseCase,
+    DeleteTodoUseCase,
+  ],
 })
 export class TodosModule {}
